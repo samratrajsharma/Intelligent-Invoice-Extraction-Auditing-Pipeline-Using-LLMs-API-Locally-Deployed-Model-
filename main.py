3b1ada8fc69671c319c643e350118ef9fda03871
Samrat Sharma <samratsharma979@gmail.com>
2026-02-20 12:26:45 +0530

    Invoice text Extraction either PDF or Image

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
+import pdfplumber
+import pytesseract
+from PIL import Image
+
 
 INVOICE_FOLDER = "invoices"
 
 
 def scan_invoices():
+
     files = os.listdir(INVOICE_FOLDER)
 
     invoice_files = []
@@ -15,14 +20,46 @@ def scan_invoices():
     return invoice_files
 
 
+def extract_text_from_pdf(path):
+
+    text = ""
+
+    with pdfplumber.open(path) as pdf:
+        for page in pdf.pages:
+            if page.extract_text():
+                text += page.extract_text() + "\n"
+
+    return text
+
+
+def extract_text_from_image(path):
+
+    img = Image.open(path)
+    return pytesseract.image_to_string(img)
+
+
+def extract_text(path):
+
+    if path.lower().endswith(".pdf"):
+        return extract_text_from_pdf(path)
+    else:
+        return extract_text_from_image(path)
+
+
 def main():
 
     invoices = scan_invoices()
 
-    print(f"Found {len(invoices)} files:")
+    for file in invoices:
 
-    for f in invoices:
-        print("-", f)
+        path = os.path.join(INVOICE_FOLDER, file)
+
+        print("=" * 50)
+        print("Processing:", file)
+
+        text = extract_text(path)
+
+        print(text)
 
 
 if __name__ == "__main__":
